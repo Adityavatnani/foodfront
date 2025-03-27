67fce77c2d3b5dd692e70a3e35cebf802bf7da0e
CodeVoyager07 <spawasthi93808@gmail.com>
2025-03-27 19:29:28 +0530

    update explore menu footer

--- src/App.jsx
+++ src/App.jsx
@@ -7,12 +7,15 @@ import PlaceOrder from './pages/PlaceOrder/PlaceOrder';
 import Footer from './components/Footer/Footer';
 import LoginPopup from './components/LoginPopup/LoginPopup';
 import ScrollButton from './components/Scroll/ScrollButton.jsx';
+//import LiveTracking from "./components/LiveTracking";
+
 
 const App = () => {
   const [showLogin, setShowLogin] = useState(false);
 
   return (
     <>
+    
       {/* Login Popup */}
       {showLogin && <LoginPopup setShowLogin={setShowLogin} />}
 
